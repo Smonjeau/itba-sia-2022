dda54ae7fedc0753cf12c928fecfa14f05c054bd
Santiago <smonjeau@itba.edu.ar>
2022-03-13 11:43:43 -0300

    add props file

--- TP1/src/main/java/Main.java
+++ TP1/src/main/java/Main.java
@@ -2,11 +2,21 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import java.util.*;
+
+
 public class Main {
+
 
 
     public static void main(String[] args) throws IOException {
 
+
+
+
+
+        Properties props = new Properties();
+        props.load(Main.class.getResourceAsStream("sia.properties"));
 
         int [][] startingState={
                 {1,5,2},
@@ -17,13 +27,29 @@ public class Main {
 
         BufferedWriter writer = new BufferedWriter(new FileWriter("logs.txt"));
 
-        writer.write("------------ BPA ------------\n");
-        Solver.bpa(startingState, writer);
-        writer.write("\n------------ BPP ------------\n");
-        Solver.bpp(startingState, writer);
-        writer.write("\n------------ BPPV ------------\n");
-        Solver.bppv(startingState,20, writer);
+
+
+        switch(props.getProperty("alg").toUpperCase()){
+            case "BPA":
+                writer.write("------------ BPA ------------\n");
+                Solver.bpa(startingState, writer);
+                break;
+            case "BPP":
+                writer.write("\n------------ BPP ------------\n");
+                Solver.bpp(startingState, writer);
+                break;
+            default:
+                throw new IllegalArgumentException("<alg> property has to be a valid option");
+        }
+
+
+
+
+
         writer.close();
+
+     //   Solver.localHeuristic(new PuzzleState(startingState),writer,new Manhattan());
+
 
     }
 
