20b9f0244801cc449d55314fc65c1dc84cab2602
jsackmann <jsackmann@itba.edu.ar>
2022-03-29 11:50:28 -0300

    TournamentSelection implemented correctly

---
+++ TP2/src/main/java/Implementations/Selection/TournamentSelection.java
@@ -0,0 +1,63 @@
+package Implementations.Selection;
+
+import entities.Individual;
+import interfaces.Selection;
+
+import java.util.*;
+
+public class TournamentSelection implements Selection {
+
+    private double tournamentChance;
+
+    public TournamentSelection(double tournamentChance) {
+        this.tournamentChance=tournamentChance;
+    }
+
+    @Override
+    public List<Individual> select(List<Individual> generation) {
+        int winnerSize=generation.size()/2;
+        List<Individual> newGeneration=new ArrayList<>(winnerSize);
+        Random random=new Random(System.currentTimeMillis());
+        List<Individual> candidateList=new ArrayList<>(4);
+        List<Individual> winnerList=new ArrayList<>(4);
+        List<Individual> loserList=new ArrayList<>(4);
+        while (newGeneration.size()<winnerSize){
+            for (int j = 0; j < 2; j++) {
+                for (int i = 0; i < 2; i++) {
+                    candidateList.add(generation.get(0));
+                    generation.remove(0);
+
+                }
+                candidateList.sort(Comparator.comparingDouble(Individual::getFitness));
+                if (random.nextDouble()>tournamentChance){
+                    winnerList.add(candidateList.get(0));
+                    loserList.add(candidateList.get(1));
+                    candidateList.clear();
+                }
+
+                else {
+                    loserList.add(candidateList.get(0));
+                    winnerList.add(candidateList.get(1));
+                    candidateList.clear();
+
+                }
+            }
+            if (random.nextDouble()>tournamentChance){
+                newGeneration.add(winnerList.get(0));
+                loserList.add(winnerList.get(1));
+                winnerList.clear();
+            }
+
+            else {
+                newGeneration.add(winnerList.get(1));
+                loserList.add(winnerList.get(0));
+                winnerList.clear();
+
+            }
+            generation.addAll(loserList);
+            loserList.clear();
+            
+        }
+        return newGeneration;
+    }
+}
