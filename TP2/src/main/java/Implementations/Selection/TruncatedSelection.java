1933f712828fbb942a93a299a4ef164ada48173c
jsackmann <jsackmann@itba.edu.ar>
2022-03-29 11:29:52 -0300

    TruncatedSelection implement correctly

--- TP2/src/main/java/Implementations/Selection/TruncatedSelection.java
+++ TP2/src/main/java/Implementations/Selection/TruncatedSelection.java
@@ -10,31 +10,14 @@ public class TruncatedSelection implements Selection {
     public List<Individual> select(List<Individual> generation) {
         int winnerSize=generation.size()/2;
 
-        //remove weight violations but checking not to remove too many
-        generation.sort(Comparator.comparingDouble(Individual::getFitness).reversed());
-        Iterator<Individual>iterator=generation.iterator();
-        int genSize= generation.size();
-//        while (iterator.hasNext()&&genSize>winnerSize){
-//            Individual ind=iterator.next();
-//            if (ind.getWeightSum()>WeightLimit){
-//                iterator.remove();
-//                genSize--;
-//            }
-//        }
-
-//        reorder from best to worst
         generation.sort(Comparator.comparingDouble(Individual::getFitness));
         Random random=new Random(System.currentTimeMillis());
 
-        //truncation value must be smaller than winner size or there wont be enough individuals next generation
-        int truncate;
-        if (genSize==winnerSize)
-            truncate=0;
-        else
-            truncate=random.nextInt(genSize-winnerSize);
+
+        int truncate=random.nextInt(winnerSize);
 
         //get rid of worst candidates
-        List<Individual>shuffledList=(generation.subList(0,genSize-truncate));
+        List<Individual>shuffledList=(generation.subList(0, generation.size()-truncate));
         //random select from the survivors
         Collections.shuffle(shuffledList);
         return shuffledList.subList(0,winnerSize);
