dc578f36397c579a0cf28a4e758bf5c8f97423b0
Juan Ignacio Sackmann Sala <jsackmann@itba.edu.ar>
2022-06-20 19:07:52 -0300

    ej1a4 y ej1b

--- TP5/algorithm.py
+++ TP5/algorithm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.optimize as sco
 import random
+import matplotlib.pyplot as plt
 
 class Autoencoder:
     def __init__(self, neurons_per_layer, train_set, output_set, beta):
@@ -49,6 +50,18 @@ class Autoencoder:
         error = np.sum(np.power(output - expected, 2))
 
         return error/2
+
+    def decode(self,input_set):
+        output=[]
+        for i in range(self.latent_index,len(self.weights)):
+            activation = np.dot(output, self.weights[i].T)
+            output = self.activation_functions[i](activation)
+        for i in range(len(output)):
+            if output[i] > 0.5 : 
+                output[i]=1 
+            else :
+                output[i]=0
+        return output
 
     def unflatten_weights(self, array):
         aux = []
@@ -129,7 +142,61 @@ def to_bin_array(encoded_caracter):
             current_row >>= 1
     return bin_array.flatten()
 
+def ej1a4():
+    generateLatentSpaceFigure()
+
+def ej1b(autoencoder):
+    total_errors=[]
+    for i in range(0, 10):
+        noise=i/(10*2)
+        error = 0
+        expected = np.array(autoencoder.output_set)
+        output=[]
+        aux=[]
+        for font in range(len(font_3)):
+            aux.append(to_bin_array(font))
+            for char in range(len(font_3[font])):
+                for char_row in range(len(font_3[font][char])):
+                    if random.random()<noise:
+                        if aux[font][char][char_row]==1:
+                            aux[font][char][char_row]=0
+                        else:
+                            aux[font][char][char_row]=1
+                        
+        
+        output=aux
+        for i in range(len(autoencoder.weights)):
+            activation = np.dot(output,autoencoder.weights[i].T)
+            output = autoencoder.activation_functions[i](activation)
+
+        error = np.sum(np.power(output - expected, 2))
+
+        total_errors.append(error)
+    plt.clf()
+    plt.plot(total_errors,np.arange(0,5,0.5))
+    plt.xlabel('Noise')
+    plt.ylabel('Total Error')
+    plt.title('Noise vs Total Error')
+    plt.show()
+       
+
+
+def generateLatentSpaceFigure(x,y,autoencoder):
+    
+    output=autoencoder.decode(x,y)
+    # output=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    # print(len(output))
+    output=np.array(output)
+    output=output.reshape(7,5)
+    plt.clf()
+    plt.imshow(output, cmap='gray')
+    plt.show()
+
 def main():
+    
+    # ej1a4()
+    # ej1b()
+
     train_set = [[1, 1], [1, 0], [0, 1], [0, 0]]
     output_set = [[1, 1], [1, 0], [0, 1], [0, 0]]
 
