bfd27eba9edf9171d16cbbd8fd7371ed72294c99
Juan Ignacio Sackmann Sala <jsackmann@itba.edu.ar>
2022-06-21 03:36:03 -0300

    ej1b fix

--- TP5/ejs.py
+++ TP5/ejs.py
@@ -22,7 +22,7 @@ def ej1b():
     all_inputs = get_input_fonts()
     autoencoder = Autoencoder(neurons_per_layer, all_inputs, all_inputs, 1)
 
-    autoencoder.weights= load_csv('TP5/weights.csv')
+    autoencoder.weights= load_csv('TP5/weights(2).csv')
 
     total_errors=[]
     for i in range(0, 10):
@@ -32,7 +32,7 @@ def ej1b():
         output=[]
         aux=[]
         for font in range(len(font_3)):
-            aux.append(to_bin_array(all_inputs[font]))
+            aux.append(all_inputs[font])
             for char in range(7):
                 for char_row in range(5):
                     if random.random()<noise:
@@ -46,6 +46,15 @@ def ej1b():
         for i in range(len(autoencoder.weights)):
             activation = np.dot(output,autoencoder.weights[i].T)
             output = autoencoder.activation_functions[i](activation)
+        
+        for font in range(len(font_3)):
+            for char in range(7):
+                for char_row in range(5):
+                    if random.random()<noise:
+                        if output[font][char*5+char_row]>0.5:
+                            output[font][char*5+char_row]=1
+                        else:
+                            output[font][char*5+char_row]=0
 
         error = np.sum(np.power(output - expected, 2))
         total_errors.append(error)
@@ -53,7 +62,6 @@ def ej1b():
 
     noise_levels = [i/(10*2) for i in range(0, 10)]
     plt.clf()
-    
     plt.plot( noise_levels,total_errors)
     plt.xlabel('Noise')
     plt.ylabel('Total Error')
@@ -63,13 +71,9 @@ def ej1b():
 def generateLatentSpaceFigure(x,y,autoencoder):
     
     output=autoencoder.decode([x,y])
-    # output=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    # print(len(output))
     output=np.array(output)
     output=output.reshape(7,5)
     plt.clf()
     plt.imshow(output, cmap='gray')
     plt.show()
 
-def main():
-    ej1a4()
